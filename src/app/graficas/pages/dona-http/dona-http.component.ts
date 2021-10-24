21b1c75f5a6aa1e4014181906640b32575e4a96d
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-24 21:09:33 +0200

    feat: dona and dona-http finished

--- src/app/graficas/pages/dona-http/dona-http.component.ts
+++ src/app/graficas/pages/dona-http/dona-http.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { Color, Label, MultiDataSet } from 'ng2-charts';
+import { GraficasService } from '../../services/graficas.service';
 
 @Component({
   selector: 'app-dona-http',
   templateUrl: './dona-http.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class DonaHttpComponent implements OnInit {
+  public doughnutChartLabels: Label[] = [
+    // 'Download Sales',
+    // 'In-Store Sales',
+    // 'Mail-Order Sales',
+    // 'Other',
+  ];
+  public doughnutChartData: MultiDataSet = [
+    // [350, 450, 100, 150]
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
+  public colors: Color[] = [
+    {
+      backgroundColor: ['#0075ED', '#00BAF7', '#00E0DB', '#00F7AD', '#00ED63'],
+    },
+  ];
+  constructor(private graficasService: GraficasService) {}
 
   ngOnInit(): void {
-  }
+    // this.graficasService.getUsuariosRedesSociales().subscribe((data) => {
+    //   console.log(data);
+    //   const labels = Object.keys(data);
+    //   // console.log(labels);
+    //   const values = Object.values(data);
+    //   // console.log(values);
+    //   this.doughnutChartLabels = labels;
+    //   this.doughnutChartData.push(values);
+    // });
 
+    this.graficasService
+      .getUsuariosRedesSocialesDonaData()
+      .subscribe(({ labels, values }) => {
+        // console.log(labels);
+        // console.log(values);
+        this.doughnutChartLabels = labels;
+        this.doughnutChartData.push(values);
+      });
+  }
 }
